cc64edd9d41d9d58506c3399efb2b9be388a0632
NovusEdge <novusedge0@gmail.com>
2022-04-15 23:54:33 +0430

    Adding main program (Incomplete/template)

---
+++ gydeon.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+// Author: Aliasgar Khimani (NovusEdge)
+// Project: github.com/NovusEdge/gydeon
+//
+// Copyright: GNU General Public License v3.0
+// See the LICENSE file for more info.
+//
+// All Rights Reserved
+*/
+
+import (
+	"fmt"
+	src "github.com/NovusEdge/gydeon/src"
+)
+
+func main() {
+	fmt.Println(src.BannerArt)
+}
