0a737db825d290d5fe49a1a4b7f841ad0231c1df
NovusEdge <novusedge@gmail.com>
2022-04-28 13:16:07 +0430

    Adding a simple uninstallation script

---
+++ uninstall.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+: '
+    Author: Aliasgar Khimani (NovusEdge)
+    Project: github.com/ARaChn3/buraq
+    Copyright: MPL 2.0
+    See the LICENSE file for more info.
+    All Rights Reserved
+'
+
+PROJECT_DIR=$(dirname $(realpath $0))
+
+printf "\033[1;36m[I]: Removing $HOME/.buraq...\033[0m\n";
+
+rm -rf $HOME/.buraq
+
+printf "\033[1;32m[+]: Done!\033[0m\n\n";
+
+
+
+printf "\033[1;36m[I]: Removing the project directory and files...\n";
+
+rm -rf $PROJECT_DIR
+
+printf "\033[1;32m[+]: Done!\033[0m\n\n";
