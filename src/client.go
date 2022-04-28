f0896fb0521b3ad4d44aa97dcc690c38cd764517
NovusEdge <novusedge@gmail.com>
2022-04-29 03:54:22 +0430

    Added simple function to check if provided credentials are valid/work

--- src/client.go
+++ src/client.go
@@ -10,7 +10,45 @@ package buraq
 // All Rights Reserved
 */
 
-// AttemptConnection makes a credentials
-func AttemptConnection() {
+import (
+	"fmt"
+	utils "github.com/NovusEdge/buraq/utils"
+	"golang.org/x/crypto/ssh"
+	"log"
+	"time"
+)
 
+// AttemptConnection tries to connect to the ssh server using credentials
+// provided.
+func AttemptConnection(proto, username, host, password string, port uint16, timeout time.Duration) (bool, error) {
+	if proto != "tcp" || proto != "udp" {
+		log.Println(utils.ColorIt(utils.ColorYellow, fmt.Sprintf("[W]: Did not recognize protocol: %s for ssh dialup.\nUsing tcp as default", proto)))
+		proto = "tcp"
+	}
+
+	sshConfig := &ssh.ClientConfig{
+		User:    username,
+		Auth:    []ssh.AuthMethod{ssh.Password(password)},
+		Timeout: timeout,
+	}
+
+	client, clientErr := ssh.Dial(proto, host, sshConfig)
+	if clientErr != nil {
+		return false, clientErr
+	}
+
+	defer client.Close()
+
+	session, sessionErr := client.NewSession()
+	if sessionErr != nil {
+		return false, sessionErr
+	}
+	defer session.Close()
+
+	out, err := session.Output("whoami")
+	if err != nil || string(out) != username {
+		return false, err
+	}
+
+	return true, nil
 }
