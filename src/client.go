99f860b55e943835b77d926bdf6f7566bfc1e7f0
NovusEdge <novusedge@gmail.com>
2022-04-28 13:17:11 +0430

    changing files to accompany the changes that come with introducing "commands/" directory

--- src/client.go
+++ src/client.go
@@ -10,40 +10,7 @@ package buraq
 // All Rights Reserved
 */
 
-import (
-	"github.com/melbahja/goph"
-)
+// AttemptConnection makes a credentials
+func AttemptConnection() {
 
-// CheckCreds returns (true, nil) if connected to ssh service successfully.
-func CheckCreds(username string, password string, target string, customConfig *goph.Config) (bool, error) {
-	var err error
-	var output []byte
-	var c *goph.Client
-
-	defer c.Close()
-
-	if customConfig == nil {
-		c, err = simpleConn(username, password, target)
-	} else {
-		c, err = customConn(customConfig)
-	}
-
-	if output, err = c.Run("whoami"); string(output) == c.Config.User {
-		return true, err
-	}
-
-	return false, err
-}
-
-// simpleConn initiates a new ssh connection with standard config.
-func simpleConn(username, password, target string) (c *goph.Client, err error) {
-	auth := goph.Password(password)
-	c, err = goph.New(username, target, auth)
-	return
-}
-
-
-// customConn initiates a new ssh connection with custom config.
-func customConn(conf *goph.Config) (*goph.Client, error) {
-	return goph.NewConn(conf)
 }
